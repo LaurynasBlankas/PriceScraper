d8693cd77fbc74100e5db7f16cc165080a1137b7
Laurynas Blankas <laurynas.blankas@gmail.com>
2026-04-09 10:54:34 +0300

    Initial commit

---
+++ the_scraper.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+from curl_cffi import requests
+
+
+url = "https://www.kaina24.lt/p/nukalkinimo-skystis-delonghi-ecodecalk-dlsc500-500-ml/"
+page = requests.get(url, impersonate="chrome110")
+
+api_key = "8712584120:AAGPZJvpnTO4cBNfs6Oyn7cS88QzOTy2oC8"
+receiver_id = "793355704"
+
+def send_telegram(message):
+    url_telegram = f"https://api.telegram.org/bot{api_key}/sendMessage"
+    payload = {"chat_id": receiver_id, "text": message}
+    requests.post(url_telegram, json=payload)
+
+soup = BeautifulSoup(page.text, "html.parser")
+low_price = float(soup.find("span", itemprop="lowPrice").text)
+
+if low_price < 8.83:
+    msg = f"Price dropped!, price is - {low_price} EUR"
+    send_telegram(msg)
+
+# elif low_price == 8.83:
+#     print(f"Price didn't change, still {low_price} EUR")
+# else:
+#     print("Price has gone up")
